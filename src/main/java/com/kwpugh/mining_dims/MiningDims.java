ecfb8c93d6ee66793ef0692fe0d31a86e4029aca
Kevin Pugh <kwpugh@comcast.net>
2021-10-29 19:10:52 -0500

    update CPA to Beta45v2

--- src/main/java/com/kwpugh/mining_dims/MiningDims.java
+++ src/main/java/com/kwpugh/mining_dims/MiningDims.java
@@ -20,11 +20,11 @@ public class MiningDims implements ModInitializer
 	public static final String MOD_ID = "mining_dims";
     public static final ItemGroup MINING_DIMS_GROUP = FabricItemGroupBuilder.build(new Identifier(MOD_ID, "mining_dims_group"), () -> new ItemStack(ItemInit.MINING_TELEPORTER));
 
-    public static final Identifier MOD_DIMENSION_ID = new Identifier(MiningDims.MOD_ID, "mining_dim");
-    public static final Identifier MOD_DIMENSION2_ID = new Identifier(MiningDims.MOD_ID, "hunting_dim");
-    public static final Identifier MOD_DIMENSION3_ID = new Identifier(MiningDims.MOD_ID, "caving_dim");
-    public static final Identifier MOD_DIMENSION4_ID = new Identifier(MiningDims.MOD_ID, "nethering_dim");
-    public static final Identifier MOD_DIMENSION5_ID = new Identifier(MiningDims.MOD_ID, "climbing_dim");
+    public static final Identifier MOD_DIMENSION_ID = new Identifier(MiningDims.MOD_ID, "mining_dim");  // 384 -64
+    public static final Identifier MOD_DIMENSION2_ID = new Identifier(MiningDims.MOD_ID, "hunting_dim"); // 384 -64
+    public static final Identifier MOD_DIMENSION3_ID = new Identifier(MiningDims.MOD_ID, "caving_dim");  // 384 -64
+    public static final Identifier MOD_DIMENSION4_ID = new Identifier(MiningDims.MOD_ID, "nethering_dim"); // 128 0
+    public static final Identifier MOD_DIMENSION5_ID = new Identifier(MiningDims.MOD_ID, "climbing_dim");  // 384 -64
 
     public static final MiningDimsConfig CONFIG = AutoConfig.register(MiningDimsConfig.class, PartitioningSerializer.wrap(JanksonConfigSerializer::new)).getConfig();
 
--- src/main/java/com/kwpugh/mining_dims/util/TeleporterUtil.java
+++ src/main/java/com/kwpugh/mining_dims/util/TeleporterUtil.java
@@ -1,9 +1,12 @@
 package com.kwpugh.mining_dims.util;
 
 import com.kwpugh.mining_dims.MiningDims;
+import com.kwpugh.mining_dims.init.EnchantmentInit;
 import com.kwpugh.mining_dims.init.MiningDimsRegistry;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -29,13 +32,16 @@ public class TeleporterUtil
         ItemStack stack1 = player.getStackInHand(hand);
         RegistryKey<World> registryKey = world.getRegistryKey();
 
+        int heightMax;
+        int heightMin;
+
+        // Only run on server side
         if (world.isClient) return TypedActionResult.success(stack);
 
         // If teleporter is enchanted, check for Returning and return to bed if so
         if(stack.hasEnchantments())
         {
-            boolean hasReturning = stack.getEnchantments().toString().contains("returning");
-            if (hasReturning && player.isSneaking())  // RETURN PLAYER TO BED
+            if(player.isSneaking() && (EnchantmentHelper.getLevel(EnchantmentInit.RETURNING, player.getEquippedStack(EquipmentSlot.MAINHAND)) > 0))
             {
                 ServerWorld serverWorld = ((ServerWorld)world).getServer().getWorld(World.OVERWORLD);
                 ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
@@ -66,10 +72,9 @@ public class TeleporterUtil
             ServerWorld destWorld = null;
             ServerWorld overWorld = ((ServerWorld)world).getServer().getWorld(World.OVERWORLD);
             ServerWorld targetWorld = ((ServerWorld)world).getServer().getWorld(dimKey);
-
             ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
-
             RegistryKey<World> currentWorldKey = world.getRegistryKey();
+
             if(currentWorldKey == dimKey)
             {
                 destWorld = overWorld;
@@ -79,6 +84,11 @@ public class TeleporterUtil
                 destWorld = targetWorld;
             }
 
+            RegistryKey<World> destKey = destWorld.getRegistryKey();
+
+            heightMax = getHeightMax(destKey);
+            heightMin = getHeightMin(destKey);
+
             // Check a number of times for a safe spot
             for (int i = 1; i < 6; i++)
             {
@@ -87,7 +97,7 @@ public class TeleporterUtil
                     serverPlayer.sendMessage((new TranslatableText("item.mining_dims.teleporter1")), true);   //checking...
                 }
 
-                if (i > 1 && i < 6)
+                if (i > 1)
                 {
                     serverPlayer.sendMessage((new TranslatableText("item.mining_dims.teleporter2")), true);
                 }
@@ -97,13 +107,13 @@ public class TeleporterUtil
 
                 // Use players current x and z for starting point
                 int x = Math.round(playerLoc.getX()) + rand.nextInt(10 + 5) - 5;
-                int y = (dimKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY3) ? 325 : 150;
+                int y = heightMax;
                 int z = Math.round(playerLoc.getZ()) + rand.nextInt(10 + 5) - 5;
 
                 Chunk chunk = destWorld.getChunk(x >> 4, z >> 4);
                 Biome biome = destWorld.getBiome(new BlockPos(x, y, z));
 
-                //Lets not dump the player in the middle of a body of water, shall we?
+                //Lets not dump the player in the middle of a body of water, shall we? Unless they really want it.
                 if (MiningDims.CONFIG.GENERAL.enableOceanCheck)
                 {
                     if((biome.getCategory().getName().equals("ocean")))
@@ -113,7 +123,7 @@ public class TeleporterUtil
                 }
 
                 //Let's avoid putting them underground
-                while (y > 20)
+                while(y > heightMin)
                 {
                     y--;
                     BlockPos groundPos = new BlockPos(x, y - 2, z);
@@ -121,11 +131,11 @@ public class TeleporterUtil
                     if (!chunk.getBlockState(groundPos).getMaterial().equals(Material.AIR) &&
                             (!chunk.getBlockState(groundPos).getBlock().equals(Blocks.BEDROCK) &&
                             (!chunk.getBlockState(groundPos).getBlock().equals(Blocks.LAVA) &&
-                            (!chunk.getBlockState(groundPos).getBlock().equals(Blocks.ACACIA_BUTTON) &&
-                            (y - 2) > 20))))
+                            //(y - 2) > 63)))
+                            (y - 2) > heightMin)))
                     {
                         // If block pos under feet is water or lava, place a stone block
-                        if(chunk.getBlockState(groundPos).getMaterial().equals(Material.WATER)) chunk.setBlockState(groundPos, Blocks.STONE.getDefaultState(), false);
+                        //if(chunk.getBlockState(groundPos).getMaterial().equals(Material.WATER)) chunk.setBlockState(groundPos, Blocks.STONE.getDefaultState(), false);
 
                         BlockPos legPos = new BlockPos(x, y - 1, z);
 
@@ -154,9 +164,45 @@ public class TeleporterUtil
         return TypedActionResult.success(stack);
     }
 
-    public void setPositionAndUpdate(ServerPlayerEntity player, World world, BlockPos bedLoc)
+    // Testing different values
+    private static int getHeightMax(RegistryKey<World> destKey)
     {
-        player.teleport(bedLoc.getX() + 0.5F, bedLoc.getY() + 0.6F, bedLoc.getZ() + 0.5F);
-        player.fallDistance = 0;
+        if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY || destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY2 || destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY5)
+        {
+            return 200;
+        }
+        else if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY3)
+        {
+            return 275;
+        }
+        else if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY4)
+        {
+            return 120;
+        }
+        else
+        {
+            return 90;
+        }
+    }
+
+    // Testing different values
+    private static int getHeightMin(RegistryKey<World> destKey)
+    {
+        if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY || destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY2 || destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY5)
+        {
+            return 40;
+        }
+        else if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY3)
+        {
+            return 80;
+        }
+        else if(destKey == MiningDimsRegistry.MININGDIMS_WORLD_KEY4)
+        {
+            return 30;
+        }
+        else
+        {
+            return 65;
+        }
     }
 }
