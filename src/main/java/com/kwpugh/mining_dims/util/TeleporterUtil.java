b9c828112eb4f274f9c80a1e71b68a21cafacbc3
Kevin Pugh <kwpugh@comcast.net>
2021-11-03 16:01:01 -0500

    updated to 21w44a

--- src/main/java/com/kwpugh/mining_dims/util/TeleporterUtil.java
+++ src/main/java/com/kwpugh/mining_dims/util/TeleporterUtil.java
@@ -3,6 +3,9 @@ package com.kwpugh.mining_dims.util;
 import com.kwpugh.mining_dims.MiningDims;
 import com.kwpugh.mining_dims.init.EnchantmentInit;
 import com.kwpugh.mining_dims.init.MiningDimsRegistry;
+import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
+import net.fabricmc.fabric.api.tag.FabricDataGeneratorTagBuilder;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
 import net.minecraft.enchantment.EnchantmentHelper;
@@ -17,7 +20,9 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.TeleportTarget;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
@@ -36,38 +41,36 @@ public class TeleporterUtil
         int heightMin;
 
         // Only run on server side
-        if (world.isClient) return TypedActionResult.success(stack);
+        if(world.isClient) return TypedActionResult.success(stack);
 
-        // If teleporter is enchanted, check for Returning and return to bed if so
-        if(stack.hasEnchantments())
+        // If teleporter is enchanted, check for Returning and return to Overworld bed if so
+        if(player.isSneaking() && (EnchantmentHelper.getLevel(EnchantmentInit.RETURNING, player.getEquippedStack(EquipmentSlot.MAINHAND)) > 0))
         {
-            if(player.isSneaking() && (EnchantmentHelper.getLevel(EnchantmentInit.RETURNING, player.getEquippedStack(EquipmentSlot.MAINHAND)) > 0))
+            ServerWorld serverWorld = ((ServerWorld)world).getServer().getWorld(World.OVERWORLD);
+            ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+
+            if(serverPlayer.getSpawnPointPosition() != null) //player bed location not null
             {
-                ServerWorld serverWorld = ((ServerWorld)world).getServer().getWorld(World.OVERWORLD);
-                ServerPlayerEntity serverPlayer = (ServerPlayerEntity) player;
+                BlockPos bedLoc = serverPlayer.getSpawnPointPosition(); //get player bed position
+                serverPlayer.stopRiding();
 
-                if(serverPlayer.getSpawnPointPosition() != null) //player bed location not null
-                {
-                    BlockPos bedLoc = serverPlayer.getSpawnPointPosition(); //get player bed position
-                    serverPlayer.stopRiding();
+                serverPlayer.teleport(serverWorld, bedLoc.getX() + 0.5F, bedLoc.getY(), bedLoc.getZ() + 0.5F, serverPlayer.getYaw(), serverPlayer.getPitch());
+                world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
 
-                    serverPlayer.teleport(serverWorld, bedLoc.getX() + 0.5F, bedLoc.getY(), bedLoc.getZ() + 0.5F, serverPlayer.getYaw(), serverPlayer.getPitch());
-                    world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ITEM_CHORUS_FRUIT_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
+                player.sendMessage((new TranslatableText("item.mining_dims.teleporter4")), true);   //Welcome Home!
 
-                    player.sendMessage((new TranslatableText("item.mining_dims.teleporter4")), true);   //Welcome Home!
+                TypedActionResult.success(stack1);
+            }
+            else
+            {
+                player.sendMessage((new TranslatableText("item.mining_dims.teleporter5")), true);  //Set a bed spawn first!
 
-                    TypedActionResult.success(stack1);
-                }
-                else
-                {
-                    player.sendMessage((new TranslatableText("item.mining_dims.teleporter5")), true);  //Set a bed spawn first!
-
-                    TypedActionResult.success(stack);
-                }
+                TypedActionResult.success(stack);
             }
         }
 
-        if(!player.isSneaking())  // RETURN PLAYER SOMEWHERE IN OVERWORLD
+        // Logic to either send to dim of teleporter in hand or back to Overworld, depending on current location
+        if(!player.isSneaking())
         {
             ServerWorld destWorld = null;
             ServerWorld overWorld = ((ServerWorld)world).getServer().getWorld(World.OVERWORLD);
@@ -107,13 +110,13 @@ public class TeleporterUtil
 
                 // Use players current x and z for starting point
                 int x = Math.round(playerLoc.getX()) + rand.nextInt(10 + 5) - 5;
-                int y = heightMax;
+                int y = heightMax;   // starting height for a given dimension
                 int z = Math.round(playerLoc.getZ()) + rand.nextInt(10 + 5) - 5;
 
                 Chunk chunk = destWorld.getChunk(x >> 4, z >> 4);
                 Biome biome = destWorld.getBiome(new BlockPos(x, y, z));
 
-                //Lets not dump the player in the middle of a body of water, shall we? Unless they really want it.
+                //Let's not dump the player in the middle of a body of water, shall we? Unless they really want it.
                 if (MiningDims.CONFIG.GENERAL.enableOceanCheck)
                 {
                     if((biome.getCategory().getName().equals("ocean")))
@@ -131,11 +134,10 @@ public class TeleporterUtil
                     if (!chunk.getBlockState(groundPos).getMaterial().equals(Material.AIR) &&
                             (!chunk.getBlockState(groundPos).getBlock().equals(Blocks.BEDROCK) &&
                             (!chunk.getBlockState(groundPos).getBlock().equals(Blocks.LAVA) &&
-                            //(y - 2) > 63)))
                             (y - 2) > heightMin)))
                     {
                         // If block pos under feet is water or lava, place a stone block
-                        //if(chunk.getBlockState(groundPos).getMaterial().equals(Material.WATER)) chunk.setBlockState(groundPos, Blocks.STONE.getDefaultState(), false);
+                        if(chunk.getBlockState(groundPos).getMaterial().equals(Material.WATER)) chunk.setBlockState(groundPos, Blocks.STONE.getDefaultState(), false);
 
                         BlockPos legPos = new BlockPos(x, y - 1, z);
 
@@ -146,9 +148,20 @@ public class TeleporterUtil
                             if (chunk.getBlockState(headPos).getMaterial().equals(Material.AIR))
                             {
                                 serverPlayer.stopRiding();
-                                serverPlayer.teleport(destWorld, x, y, z, serverPlayer.getYaw(), serverPlayer.getPitch());
-                                serverPlayer.fallDistance = 0.0F;
+                                //serverPlayer.teleport(destWorld, x, y, z, serverPlayer.getYaw(), serverPlayer.getPitch());
 
+
+
+                                // TEST CODE
+                                Vec3d destVec = new Vec3d(x, y, z);
+                                TeleportTarget teleportTarget = new TeleportTarget(destVec, null, player.getYaw(), player.getPitch());
+                                FabricDimensions.teleport(serverPlayer, destWorld, teleportTarget);
+                                // TEST CODE
+
+
+
+
+                                serverPlayer.fallDistance = 0.0F;
                                 world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1.0F, 1.0F);
 
                                 return TypedActionResult.success(stack);
